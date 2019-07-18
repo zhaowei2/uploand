27fbbfa74d1a8f3454b477a7fbe5b852839263c3
zhaowei <840728393@qq.com>
2019-07-18 15:37:56 +0800

    add

---
+++ upload/src/components/upload.vue
@@ -0,0 +1,95 @@
+<script>
+function noop() {}
+export default {
+  name: 'ElUpload',
+  props:{
+     action: {
+      type: String,
+      required: true
+    },
+     headers: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    data: Object,
+    multiple: Boolean,
+    name: {
+      type: String,
+      default: 'file'
+    },
+    drag: Boolean,
+    dragger: Boolean,
+    withCredentials: Boolean,
+    showFileList: {
+      type: Boolean,
+      default: true
+    },
+    accept: String,
+    type: {
+      type: String,
+      default: 'select'
+    },
+    beforeUpload: Function,
+    beforeRemove: Function,
+    onRemove: {
+      type: Function,
+      default: noop
+    },
+    onChange: {
+      type: Function,
+      default: noop
+    },
+    onPreview: {
+      type: Function
+    },
+    onSuccess: {
+      type: Function,
+      default: noop
+    },
+    onProgress: {
+      type: Function,
+      default: noop
+    },
+    onError: {
+      type: Function,
+      default: noop
+    },
+    fileList: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    autoUpload: {
+      type: Boolean,
+      default: true
+    },
+    listType: {
+      type: String,
+      default: 'text' // text,picture,picture-card
+    },
+    httpRequest: Function,
+    disabled: Boolean,
+    limit: Number,
+    onExceed: {
+      type: Function,
+      default: noop
+    }
+  },
+  data() {
+    return {
+      uploadFiles: [],
+      dragOver: false,
+      draging: false,
+      tempIndex: 1
+    };
+  },
+  computed: {
+    uploadDisabled() {
+      return this.disabled || (this.elForm || {}).disabled;
+    }
+  },
+}
+</script>
